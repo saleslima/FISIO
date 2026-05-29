d80c4938125b28a1f88661fbe3936bf681af3772
saleslima <156296179+saleslima@users.noreply.github.com>
2026-05-29 03:51:37 -0300

    Increase animation duration from 3000ms to 6000ms

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ function startLogoIntroAnimation() {
             offset: 1
         }
     ], {
-        duration: 3000,
+        duration: 6000,
         easing: 'cubic-bezier(0.22, 1, 0.36, 1)',
         fill: 'forwards'
     });
